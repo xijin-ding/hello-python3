763e967702f7100ca30b6c276e57d7ae1b5bd45c
xijin-ding <43772995+xijin-ding@users.noreply.github.com>
2020-03-05 16:29:14 +0800

    Revert "git.02 我上瘾了hell,git"
    This reverts commit c30d9616bfaccf7cc16b8f8c7b853fc456f5c894.

---
+++ git02.py
@@ -0,0 +1,10 @@
+# 还是很喜欢这个软件的，什么软件呢？他的名字叫“pycharm” 设计角度特别好，交互也是非常棒的。
+for i in range(1,10):
+    print()
+    for j in range(1,i+1):
+        print("%d*%d=%d" %(i,j,i*j),end=" ")
+
+
+print("希望这次测试能领悟到更多！！！，hello,Pycharm")
+print("加一个注释吧，这样更容易分辨出来！")
+print("我上瘾了，hello git")

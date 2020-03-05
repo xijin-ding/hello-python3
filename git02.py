f1ce025d92a05fe25af822b038f61b714a151064
xijin-ding <43772995+xijin-ding@users.noreply.github.com>
2020-03-05 13:37:36 +0800

    Revert "Update github_test01.py"
    This reverts commit c07d9c7f8b6ce2adb04dfb2d2db4c64876779644.
    
    # Conflicts:
    #	github_test01.py

---
+++ git02.py
@@ -0,0 +1,8 @@
+# 还是很喜欢这个软件的，什么软件呢？他的名字叫“pycharm” 设计角度特别好，交互也是非常棒的。
+for i in range(1,10):
+    print()
+    for j in range(1,i+1):
+        print("%d*%d=%d" %(i,j,i*j),end=" ")
+
+
+print("希望这次测试能领悟到更多！！！，hello,Pycharm")

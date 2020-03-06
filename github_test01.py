81653172bf0ac3674fe0360fbb4fc54186b01bc5
xijin-ding <43772995+xijin-ding@users.noreply.github.com>
2020-03-06 11:20:23 +0800

    恢复测试

---
+++ github_test01.py
@@ -0,0 +1,14 @@
+print("hell-python3")
+# 99**cfb
+import time
+
+for i in range(1,10):
+    print()
+    for j in range(1,i+1):
+        print("%d*%d=%d" %(i,j,i*j),end=" ")
+
+
+time.sleep(3)
+print("这个是第一文件，关于github测试")
+print("hello ,shanghai ")
+print("发现gitdesktop,检查到文件有变动")
---
+++ github_test02.py
@@ -0,0 +1,9 @@
+# test02是为了测试github的使用方法，希望能掌控的更好！
+for i in range(1,10):
+    print()
+    for j in range(1,i+1):
+        print("%d*%d=%d" %(i,j,i*j),end=" ")
+
+
+print("这是第二个github测试文件，good")
+print("测试一下test02,01快受不了我们了")

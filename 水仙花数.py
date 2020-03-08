83fe4583958f73ce1d876e9ea5af56c3d5000290
xijin-ding <43772995+xijin-ding@users.noreply.github.com>
2020-03-09 00:00:59 +0800

    水仙花数，添加颜色提示

--- 水仙花数.py
+++ 水仙花数.py
@@ -2,16 +2,16 @@
 
 import time
 for a in range(1, 10):
-    time.sleep(0.01)
+    time.sleep(0.02)
     print("\033[0;27;40m\t执行a循环\033[0m","第"+str(a)+"次")
     a1 = a ** 3
     for b in range(0, 10):
-        time.sleep(0.01)
+        time.sleep(0.02)
         print("\033[0;31;40m\t执行b循环\033[0m","第"+str(b)+"次")
         b1 = b ** 3
         for c in range(0, 10):
             c1 = c ** 3
-            time.sleep(0.01)
+            time.sleep(0.02)
             print("执行c循环","第"+str(c)+"次")
             if a1+b1+c1==a*100+b*10+c:
                 print("\033[0;34;0m\t%d\033[0m" %(a*100+b*10+c))
